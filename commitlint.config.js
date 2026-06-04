77d0ebdc2a8a16086dfef0f9f957f14f1dad0623
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2026-06-04 21:21:46 +0200

    chore: add commitlint config for conventional commits

---
+++ commitlint.config.js
@@ -0,0 +1,36 @@
+/** @type {import('@commitlint/types').UserConfig} */
+const config = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "subject-case": [
+      2,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+    ],
+    "header-max-length": [2, "always", 120],
+    "scope-case": [2, "always", "lower-case"],
+  },
+};
+
+module.exports = config;
